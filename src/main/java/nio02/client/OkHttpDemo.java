283591215d45d3bea29fc1b218a470a1a8575623
Syuan <siyuan.liu@xiaobao100.com>
2021-08-15 23:58:54 +0800

    chore: update

---
+++ src/main/java/nio02/client/OkHttpDemo.java
@@ -0,0 +1,28 @@
+package nio02.client;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+
+/**
+ * @Author: Syuan
+ * @Date: 2021/8/15 11:47 下午
+ */
+public class OkHttpDemo {
+
+    public static void main(String[] args) {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("http://localhost:8801/")
+                .build();
+        try {
+            Response response = client.newCall(request).execute();
+            System.out.println(response.body().toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
